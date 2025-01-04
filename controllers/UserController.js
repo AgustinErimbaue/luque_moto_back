e352848abc7f0de637965b0298b916c3a15b90b6
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2025-01-04 18:19:21 +0100

    feat(users): add user creation controller and route setup
    - Created 'users.js' in the 'routes' folder to define user-related routes
    - Added a controller function for user creation
    - Linked the controller to the route for better modularity

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,15 @@
 const { User } = require("../models/index");
 
-const UserController = {};
+const UserController = {
+  async create(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.status(201).send({ msg: "Usuario creado con exito", user });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  },
+};
 
 module.exports = UserController;
