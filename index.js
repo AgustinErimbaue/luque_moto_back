40d267db70edd5dd1313575d4f28fe200ddb5e3d
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2025-01-04 18:24:25 +0100

    feat(users): integrate user creation route and controller
    - Added user creation route in 'routes/users.js' with POST method
    - Linked the route to the UserController 'create' function
    - Registered the '/users' route in 'index.js' to enable endpoint usage

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 
+app.use("/users", require("./routes/users"));
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
