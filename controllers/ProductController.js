1dbc7ec27acd24f934ec5a2518d1476ae4c32053
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2025-01-05 14:13:01 +0100

    feat: Implement product retrieval endpoint (getAll)
    - Added  function to  to fetch all products.
    - Modified  route in  to use .
    - Improved error handling in  to send a more informative response.

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -4,7 +4,16 @@ const ProductController = {
   async create(req, res) {
     try {
       const product = await Product.create(req.body);
-      res.status(201).send({ msg: "PProducto creado", product });
+      res.status(201).send({ msg: "Producto creado", product });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  },
+  async getAll(req, res) {
+    try {
+      const products = await Product.findAll();
+      res.send({ msg: "Todos los productos:", products });
     } catch (error) {
       console.error(error);
       res.status(500).send(error);
