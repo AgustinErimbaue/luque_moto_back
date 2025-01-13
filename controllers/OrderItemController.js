e7bab1773ed8d78c5d0ecc8c67cfd9193f0bf5ee
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2025-01-13 12:57:26 +0100

    feat: add CRUD endpoints for OrderItem and integrate routes
    - Created OrderItemController with the following methods:
      - : To create a new order item.
      - : To fetch all order items.
      - : To fetch a specific order item by ID.
      - : To update an order item.
      - : To delete an order item.
    
    - Created a new route file for OrderItem and added routes:
      -  for creating order items.
      -  for fetching all order items.
      -  for fetching a specific order item.
      -  for updating an order item.
      -  for deleting an order item.
    
    - Integrated OrderItem routes in  with:

--- controllers/OrderItemController.js
+++ controllers/OrderItemController.js
@@ -1,6 +1,64 @@
+const { where } = require("sequelize");
 const { OrderItem, Sequelize } = require("../models/index");
 const { Op } = Sequelize;
 
-const OrderItemController = {};
+const OrderItemController = {
+  async create(req, res) {
+    try {
+      const orderItem = await OrderItem.create(req.body);
+      res.status(201).send({ msg: "Se crea correctamente", orderItem });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  },
+  async getAll(req, res) {
+    try {
+      const ordersItems = await OrderItem.findAll();
+      res
+        .status(201)
+        .send({ msg: "Busqueda realizada con exito", ordersItems });
+    } catch (error) {}
+  },
+  async getById(req, res) {
+    try {
+      const order = await OrderItem.findOne({
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.status(201).send({ msg: "Orden encontrada con exito", order });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  },
+  async updateOrder(req, res) {
+    try {
+      const order = await OrderItem.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.status(201).send({ msg: "Orden modificada con exito", order });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  },
+  async deleteOrder(req, res) {
+    try {
+      await OrderItem.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.send("Producto eliminado correctamente");
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  },
+};
 
 module.exports = OrderItemController;
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(express.json());
 app.use("/users", require("./routes/users"));
 app.use("/products", require("./routes/products"));
 app.use("/orders", require("./routes/order"));
+app.use("/orderitem", require("./routes/orderItem"))
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
