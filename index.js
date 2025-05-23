4f063bb06b3e5e149340603d6ef9d30edb45f169
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2025-05-23 12:16:03 +0200

    Corrijo configuración de CORS para permitir origen desde Vercel correctamente

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const express = require("express");
-const app = express();
-require("dotenv").config();
-const PORT = process.env.PORT || 3000;
 const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 
-const corsOptions = {
-  origin: 'https://luque-moto-front.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-};
+const allowedOrigins = [
+  'https://luque-moto-front.vercel.app',
+  'https://luque-moto-front-drqr8nquo-agustinerimbaues-projects.vercel.app'
+];
 
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: function (origin, callback) {
+    console.log("Request from origin:", origin);
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: false, 
+}));
 
 app.use(express.json());
+
+
 app.use("/users", require("./routes/users"));
 app.use("/products", require("./routes/products"));
 app.use("/orders", require("./routes/orders"));
