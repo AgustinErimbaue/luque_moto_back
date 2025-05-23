c3d54f623e765447593673faa782ca26ecf1fd89
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2025-05-23 11:23:08 +0200

    correccion de bug

--- index.js
+++ index.js
@@ -4,15 +4,9 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 
+app.use(cors());
+
 app.use(express.json());
-app.use(cors({
-  origin: [
-    "https://luque-moto-front.vercel.app", 
-    "https://luque-moto-front-egzio8gqy-agustinerimbaues-projects.vercel.app", 
-    "http://localhost:3000"
-  ],
-  credentials: true
-}));
 app.use("/users", require("./routes/users"));
 app.use("/products", require("./routes/products"));
 app.use("/orders", require("./routes/orders"));
